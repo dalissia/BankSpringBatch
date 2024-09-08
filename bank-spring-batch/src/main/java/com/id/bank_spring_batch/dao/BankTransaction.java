e423b20ac8768fef43f27faa09e2b713294e6e60
dalissia <dalissiabenidir@gmail.com>
2024-09-08 11:49:45 +0200

    First commit SpringBatch

--- bank-spring-batch/src/main/java/com/id/bank_spring_batch/dao/BankTransaction.java
+++ bank-spring-batch/src/main/java/com/id/bank_spring_batch/dao/BankTransaction.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
